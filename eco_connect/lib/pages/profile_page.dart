022792bcb15f3d1f7f2aaf71a5d9337c57c6fb92
Maitry Parikh <maitryparikh23@gmail.com>
2024-02-11 19:30:49 +0530

    Updated profile page

--- eco_connect/lib/pages/profile_page.dart
+++ eco_connect/lib/pages/profile_page.dart
@@ -1,8 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eco_connect/components/drawer.dart';
+import 'package:eco_connect/components/my_textfield.dart';
+import 'package:eco_connect/components/post_ui.dart';
 import 'package:eco_connect/components/text_box.dart';
 import 'package:eco_connect/components/my_button.dart';
+import 'package:eco_connect/components/user_post_card.dart';
+import 'package:eco_connect/helper/helper_method.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -12,8 +18,7 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-
-  // get the current user
+// get the current user
   final currentUser = FirebaseAuth.instance.currentUser!;
 
   // all users
@@ -64,97 +69,148 @@ class _ProfilePageState extends State<ProfilePage> {
       }
   }
 
+  // open url
+  Future<void> _launchURL(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    } else {
+      print('yes');
+      await launchUrl(url);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
-        title: Text("Profile Page"),
         backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-      ),
+          ),
+      
+      body: Center(
+        child: Column(
+          children: [
+                Expanded(
+                  child: StreamBuilder<DocumentSnapshot>(
+                    stream:FirebaseFirestore.instance.collection('Users').doc(currentUser.email).snapshots(),
+                    builder: (context, snapshot) {
+                      // get user data
+                      if(snapshot.hasData) {
+                        final userData = snapshot.data!.data() as Map<String, dynamic>;
+                        return ListView(
+                  children: [
+                    const SizedBox(height: 50,),
 
-      body:StreamBuilder<DocumentSnapshot>(
-        stream:FirebaseFirestore.instance.collection('Users').doc(currentUser.email).snapshots(),
-        builder: (context, snapshot) {
-          // get user data
-          if(snapshot.hasData) {
-            final userData = snapshot.data!.data() as Map<String, dynamic>;
-            return ListView(
-              children: [
-                const SizedBox(height: 50,),
-
-                // profile pic
-                Icon(
-                  Icons.person,
-                  size: 72,
-                  color: Theme.of(context).appBarTheme.backgroundColor,
-                ),
-
-
-                const SizedBox(height: 10,),
-                // user email
-                Text(
-                  currentUser.email!,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.grey[700]),
-                ),
-
-                const SizedBox(height: 10,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:[
-                    MyButton(
-                      text: "Expert",
-                      onTap: () {},
+                    // profile pic
+                    Icon(
+                      Icons.person,
+                      size: 72,
+                      color: Theme.of(context).appBarTheme.backgroundColor,
                     ),
-                const SizedBox(height: 10,),
-                    MyButton(
-                      text: "Industry",
-                      onTap: () {},
+
+
+                    const SizedBox(height: 10,),
+                    // user email
+                    Text(
+                      currentUser.email!,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.grey[700]),
                     ),
-                  ]
-                ),
 
-                const SizedBox(height: 30,),
+                    const SizedBox(height: 10,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:[
+                        MyButton(
+                          text: "Expert",
+                          onTap: () => _launchURL(Uri. parse('https://google.com')),
+                        ),
+                    const SizedBox(height: 10,),
+                        MyButton(
+                          text: "Industry",
+                          onTap: () {},
+                        ),
+                      ]
+                    ),
 
-                // user details
-                Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: Text('My Details', 
-                  style: TextStyle(color: Colors.grey[600]),
-                ),
-                ),
+                    const SizedBox(height: 30,),
 
-                // username
-                MyTextBox(
-                  text: userData['username'],
-                  sectionName: 'username',
-                  onPressed: () => editField('username'),
-                ),
+                    // user details
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0),
+                      child: Text('My Details', 
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    ),
 
-                // bio
-                MyTextBox(
-                  text: userData['bio'],
-                  sectionName: 'bio',
-                  onPressed: () => editField('bio'),
-                ),
+                    // username
+                    MyTextBox(
+                      text: userData['username'],
+                      sectionName: 'username',
+                      onPressed: () => editField('username'),
+                    ),
 
-                const SizedBox(height: 50,),
+                    // bio
+                    MyTextBox(
+                      text: userData['bio'],
+                      sectionName: 'bio',
+                      onPressed: () => editField('bio'),
+                    ),
 
-                // users posts
-                Padding(
-                  padding: const EdgeInsets.only(left: 30.0),
-                  child: Text('My Posts', 
-                  style: TextStyle(color: Colors.grey[600]),
+                    const SizedBox(height: 50,),
+
+                    // users posts
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30.0),
+                      child: Text('My Posts', 
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    ),
+                    StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection("User Posts")
+                      .where('UserEmail', isEqualTo: currentUser.email)
+                      .orderBy("TimeStamp", descending: false)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          // get message
+                          final post = snapshot.data!.docs[index];
+                          return UserPost(
+                            message: post["Message"],
+                            user: post["UserEmail"],
+                            postId: post.id,
+                            time: formatDate(post['TimeStamp']),
+                            likes: List<String>.from(post['Likes'] ?? []),
+                            commentCount: post['commentCount'],
+                          );
+                        },
+                      );
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error${snapshot.error}'));
+                    }
+                    return Center(child: CircularProgressIndicator());
+                  },
                 ),
-                ),
-              ],
-            ); 
-          } else if(snapshot.hasError) {
+                SizedBox(height: 30,),
+                  ],
+                ); 
+                  } else if(snapshot.hasError) {
             return Center(child: Text('Error${snapshot.error}'));
           }
           return const Center(child: CircularProgressIndicator(),);
-        }
+                }
+              ),
+            ),
+            
+            
+          ],
+        ),
       )
     );
   }
